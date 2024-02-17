acf744d61499823159c4fcc30838f34130f3b3f1
Garrett Bland <gmorganbland@gmail.com>
2024-02-16 22:24:27 -0600

    feat: netlify edge function preset

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,10 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
+    /**
+     * Use Netlify Edge functions
+     * https://nitro.unjs.io/deploy/providers/netlify#netlify-edge-functions
+     */
+    preset: 'netlify-edge',
     runtimeConfig: {
         /**
          * "dev_token" by default. Use NEXT_API_TOKEN to override in .env
